7fe329bf6d31df7617f7705211708761e8c83c4a
Be-Ge <bereketgebreyesusnl@gmail.com>
2024-03-16 10:20:24 +0100

    Using API's week-1-prep-ex done!

--- Week1/prep-exercises/1-catwalk-promises/index.js
+++ Week1/prep-exercises/1-catwalk-promises/index.js
@@ -12,6 +12,15 @@ function walk(img, startPos, stopPos) {
     // `stopPos`.
     // Make good use of the `STEP_INTERVAL_PX` and `STEP_INTERVAL_MS`
     // constants.
+    let currentPos = startPos;
+    const moveInterval = setInterval(() => {
+      currentPos += STEP_SIZE_PX;
+      img.style.left = currentPos + 'px';
+      if (currentPos >= stopPos) {
+        clearInterval(moveInterval);
+        resolve();
+      }
+    }, STEP_INTERVAL_MS);
   });
 }
 
@@ -21,6 +30,12 @@ function dance(img) {
     // and, after a timeout, reset the `img` back to the walking cat. Then
     // resolve the promise.
     // Make good use of the `DANCING_CAT_URL` and `DANCE_TIME_MS` constants.
+    const originalSrc = img.src;
+    img.src = DANCING_CAT_URL;
+    setTimeout(() => {
+      img.src = originalSrc;
+      resolve();
+    }, DANCE_TIME_MS);
   });
 }
 
@@ -35,6 +50,14 @@ function catWalk() {
   // 2. Then dance for 5 secs.
   // 3. Then walk from `centerPos` to `stopPos`.
   // 4. Repeat the first three steps indefinitely.
+  function catWalkLoop() {
+    walk(img, startPos, centerPos)
+      .then(() => dance(img))
+      .then(() => walk(img, centerPos, stopPos))
+      .then(catWalkLoop);
+  }
+
+  catWalkLoop();
 }
 
 window.addEventListener('load', catWalk);
